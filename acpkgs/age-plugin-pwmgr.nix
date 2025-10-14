345408657691663cf438b97d05612e798782a7cc
Antoine Carnec <antoinecarnec@gmail.com>
2025-10-14 12:08:21 +0100

    update age-plugin-pwgmr

--- acpkgs/age-plugin-pwmgr.nix
+++ acpkgs/age-plugin-pwmgr.nix
@@ -10,8 +10,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "antotocar34";
     repo = "age-plugin-pwmgr";
-    rev = "b355dc1b70ce61317feece94a034f417091595e0";
-    sha256 = "sha256-v7VLWAwbEZAUVgvBtpPmvNw2OZHJRnQxSzz0bH0qzOg=";
+    rev = "393185f3ff00085ee34f94df83016bbf867c001c";
+    sha256 = "sha256-tM3BwQp3YCmp71AzwsSzR/urxhnOq96pkDHNE1fWHjw=";
   };
 
   cargoHash = "sha256-Tc6K4HaoPJ6j6Px6pjgvSxlamvhUgMokHZl8tUOOSEo=";
